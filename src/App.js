4ba27a1205db7376d0d18a4065f69ed591207076
Henry <aguhenrychuks@gmail.com>
2022-10-28 22:15:24 +0100

    added the linked images that concluded the profile link section

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import "./App.css";
 
 // Images
 import profile_img from "./images/profile__img.png";
+import shareImage from "./images/share.png";
+import menuImage from "./images/menu.png";
+import slack from "./images/slack.png";
+import github from "./images/github.png";
 function App() {
   return (
     <div className="container">
+      <div className="navigation-icons">
+        <img src={shareImage} alt="shareImage" className="share" />
+        <img src={menuImage} alt="menuImage" className="menu" />
+      </div>
       <div className="profile-header">
         <img src={profile_img} alt="profile_img" id="profile__img" />
         <b>Annette Black</b>
@@ -13,10 +21,31 @@ function App() {
       <div className="profile-links">
         <a href="https://twitter.com/_henryi">Twitter Link</a>
         <a href="https://training.zuri.team/">Zuri Team</a>
-        <a href="http://books.zuri.team/">Zuri Books</a>
-        <a href="https://books.zuri.team/">Python Books</a>
-        <a href="https://background.zuri.team/">Background Check for Coders</a>
-        <a href="https://books.zuri.team/design-rules">Design Books</a>
+        <a href="http://books.zuri.team/" id="books">
+          Zuri Books
+        </a>
+        <a href="https://books.zuri.team/" id="book__python">
+          Python Books
+        </a>
+        <a href="https://background.zuri.team/" id="pitch">
+          Background Check for Coders
+        </a>
+        <a href="https://books.zuri.team/design-rules" id="book__design">
+          Design Books
+        </a>
+      </div>
+      <div className="profile-socials">
+        <a
+          href="https://hng9.slack.com"
+        >
+          <img src={slack} alt="slack" />
+        </a>
+        <a href="https://github.com/HenryAgu">
+          <img src={github} alt="github" />
+        </a>
+      </div>
+      <div className="sponsors">
+        
       </div>
     </div>
   );
