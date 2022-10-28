09280fb57f03f256293c33d36f1ff04fd4ca52be
Henry <aguhenrychuks@gmail.com>
2022-10-28 20:14:56 +0100

    profile-header done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import "./App.css"
+import profile_img from "./images/profile__img.png"
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
+    <div className="container">
+        <div className="profile-header">
+          <img src={profile_img} alt="profile_img" id="profile__img"/>
+          <b>Annette Black</b>
+        </div>
     </div>
   );
 }
