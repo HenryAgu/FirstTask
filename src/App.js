4a316fdc4fd829b46ab1c78a808f0560d95cc6da
Henry <aguhenrychuks@gmail.com>
2022-10-28 21:16:25 +0100

    added the URL links to the links in profile links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import "./App.css"
-import profile_img from "./images/profile__img.png"
+// stylesheet
+import "./App.css";
+
+// Images
+import profile_img from "./images/profile__img.png";
 function App() {
   return (
     <div className="container">
-        <div className="profile-header">
-          <img src={profile_img} alt="profile_img" id="profile__img"/>
-          <b>Annette Black</b>
-        </div>
+      <div className="profile-header">
+        <img src={profile_img} alt="profile_img" id="profile__img" />
+        <b>Annette Black</b>
+      </div>
+      <div className="profile-links">
+        <a href="https://twitter.com/_henryi">Twitter Link</a>
+        <a href="https://training.zuri.team/">Zuri Team</a>
+        <a href="http://books.zuri.team/">Zuri Books</a>
+        <a href="https://books.zuri.team/">Python Books</a>
+        <a href="https://background.zuri.team/">Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules">Design Books</a>
+      </div>
     </div>
   );
 }
