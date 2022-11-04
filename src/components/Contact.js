90da6183418b7fd57a6740565ca64fbcef88e7ca
Henry <aguhenrychuks@gmail.com>
2022-11-04 22:10:37 +0100

    issue: ternary operator not working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,28 @@
 // React Helmet
 import { Helmet } from "react-helmet";
 
+// React Hook
+import React, { useState } from "react";
+
 // stylesheet
 import "./Contact.css";
 const Contact = () => {
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [error, setError] = useState(false);
+  const submitHandler = (event) => {
+    if (
+      firstName.length == " " ||
+      lastName.length == " " ||
+      email.length == " " ||
+      message.length == " "
+    ) {
+      event.preventDefault();
+      setError(true);
+    }
+  };
   return (
     <>
       <Helmet>
@@ -16,7 +35,7 @@ const Contact = () => {
           <h1>Contact Me</h1>
           <p>Hi there, contact me to ask me about anything you have in mind.</p>
         </div>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={submitHandler}>
           <div className="form-name">
             <div className="first-name">
               <label>First Name</label>
@@ -25,8 +44,14 @@ const Contact = () => {
                 type="text"
                 placeholder="Enter your first name"
                 id="first_name"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
               />
-              <span>This is a hint text to help user.</span>
+              {error && firstName.length <= 0 ? (
+                <span>This is a hint text to help user.</span>
+              ) : (
+                ""
+              )}
             </div>
             <div className="last-name">
               <label>Last Name</label>
@@ -35,15 +60,32 @@ const Contact = () => {
                 type="text"
                 placeholder="Enter your last name"
                 id="last_name"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
               />
-              <span>This is a hint text to help user.</span>
+              {error && lastName.length <= 0 ? (
+                <span>This is a hint text to help user.</span>
+              ) : (
+                ""
+              )}
             </div>
           </div>
           <div className="email">
             <label>Email</label>
             <br />
-            <input type="email" id="email" placeholder="yourname@email.com" />
-            <span>This is a hint text to help user.</span>
+            <input
+              type="email"
+              id="email"
+              placeholder="yourname@email.com"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            {error && email.length <= 0 ? (
+              <span>This is a hint text to help user.</span>
+            ) : (
+              ""
+            )}
+            
           </div>
           <div className="message">
             <label>Message</label>
@@ -51,8 +93,15 @@ const Contact = () => {
             <textarea
               id="message"
               placeholder="Send me a message and I'll reply you as soon as possible..."
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              className={error && message.length <= 0 ? "special-message-error":"textarea"}
             ></textarea>
-            <span className="message-error">Please enter a message</span>
+            {error && message.length <= 0 ? (
+              <span className="message-error">Please enter a message</span>
+            ) : (
+              ""
+            )}
           </div>
           <div className="check">
             <input type="checkbox" />
