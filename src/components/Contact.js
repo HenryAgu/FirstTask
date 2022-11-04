16e18e894308fa8b4dabef7cb7324cab215c46b3
Henry <aguhenrychuks@gmail.com>
2022-11-04 12:25:22 +0100

    feature: completed more aspect of the form excluding focus styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,6 +26,7 @@ const Contact = () => {
                 placeholder="Enter your first name"
                 id="first_name"
               />
+              <span>This is a hint text to help user.</span>
             </div>
             <div className="last-name">
               <label>Last Name</label>
@@ -35,8 +36,31 @@ const Contact = () => {
                 placeholder="Enter your last name"
                 id="last_name"
               />
+              <span>This is a hint text to help user.</span>
             </div>
           </div>
+          <div className="email">
+            <label>Email</label>
+            <br />
+            <input type="email" id="email" placeholder="yourname@email.com" />
+            <span>This is a hint text to help user.</span>
+          </div>
+          <div className="message">
+            <label>Message</label>
+            <br />
+            <textarea
+              id="message"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+            ></textarea>
+            <span>Please enter a message</span>
+          </div>
+          <div className="check">
+            <input type="checkbox" />
+            <p>
+              You agree to providing your data to Henry who may contact you.
+            </p>
+          </div>
+          <button>Send message</button>
         </form>
       </div>
     </>
